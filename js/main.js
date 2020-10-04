cf016b35c9d81c6d4e1cac57273f726361ed20ca
Alexandre Massé <alexandre.masse94@gmail.com>
2020-10-04 18:46:08 +0200

    :sparkles: add dat.GUI

--- js/main.js
+++ js/main.js
@@ -1,11 +1,28 @@
 import mapboxgl from 'mapbox-gl'
 import PathFinder from 'geojson-path-finder'
 import * as turf from '@turf/turf'
-import { throttle } from 'lodash'
+import * as dat from 'dat.gui'
+import {throttle} from 'lodash'
 
 import geoJsonRoutes from '../data/routes.json'
 
+// DOM
+
 const info = document.querySelector('.info')
+
+// CONF
+
+const configuration = {
+  showTiles: false,
+  pathType: 'curved',
+  pathWidth: 8,
+  pathColor: '#000000',
+  pathSimplification: 0.00004,
+  pathCurveSharpness: 0.4,
+  routesWidth: 3,
+  routesColor: '#98bafa',
+}
+
 
 // MAP
 
@@ -22,7 +39,24 @@ map.on('load', () => {
 
   // tiles
 
-  // TODO
+  map.addSource('tiles', {
+    type: 'raster',
+    tiles: [
+      process.env.CUSTOM_TILES_URL
+    ],
+    tileSize: 256,
+  })
+
+  map.addLayer({
+    id: 'tiles',
+    source: 'tiles',
+    type: 'raster',
+    layout: {
+      visibility: configuration.showTiles ? 'visible' : 'none',
+    },
+    minzoom: 13,
+    maxZoom: 20,
+  })
 
   // routes
 
@@ -39,9 +73,8 @@ map.on('load', () => {
       'line-cap': 'round',
     },
     paint: {
-      'line-color': '#98bafa',
-      'line-opacity': 1,
-      'line-width': 2
+      'line-color': configuration.routesColor,
+      'line-width': configuration.routesWidth,
     }
   })
 
@@ -60,11 +93,14 @@ map.on('load', () => {
       'line-cap': 'round',
     },
     paint: {
-      'line-color': '#404040',
-      'line-opacity': 1,
-      'line-width': 5
+      'line-color': configuration.pathColor,
+      'line-width': configuration.pathWidth
     }
   })
+
+  // calculate path
+
+  updatePath()
 })
 
 // MARKERS
@@ -72,14 +108,16 @@ map.on('load', () => {
 // start markers
 
 const markerStartOnLine = new mapboxgl.Marker({
-  color: "#c5f1b5"
+  color: '#c5f1b5',
+  scale: 0.8
 })
   .setLngLat([0, 0])
   .addTo(map)
 
 const markerStart = new mapboxgl.Marker({
   draggable: true,
-  color: "#2cbe06"
+  color: '#2cbe06',
+  scale: 1
 })
   .setLngLat([2.5731, 49.1360])
   .addTo(map)
@@ -87,14 +125,16 @@ const markerStart = new mapboxgl.Marker({
 // end markers
 
 const markerEndOnLine = new mapboxgl.Marker({
-  color: "#f1b5b5"
+  color: '#f1b5b5',
+  scale: 0.8
 })
   .setLngLat([0, 0])
   .addTo(map)
 
 const markerEnd = new mapboxgl.Marker({
   draggable: true,
-  color: "#be061b"
+  color: '#be061b',
+  scale: 1
 })
   .setLngLat([2.5699, 49.1340])
   .addTo(map)
@@ -102,11 +142,11 @@ const markerEnd = new mapboxgl.Marker({
 
 // PATH
 
-const geoJsonRoutesChunked = turf.lineChunk(geoJsonRoutes, 0.00005,{ units: 'kilometers'} )
+const geoJsonRoutesChunked = turf.lineChunk(geoJsonRoutes, 0.00005, {units: 'kilometers'})
 
-const pathFinder = new PathFinder(geoJsonRoutesChunked, { precision: 0.00001 })
+const pathFinder = new PathFinder(geoJsonRoutesChunked, {precision: 0.00001})
 
-const routesMultiLineString = turf.multiLineString(geoJsonRoutes.features.map(feature => feature.geometry.coordinates));
+const routesMultiLineString = turf.multiLineString(geoJsonRoutes.features.map(feature => feature.geometry.coordinates))
 
 const updatePath = () => {
 
@@ -128,7 +168,10 @@ const updatePath = () => {
   // calculate path
 
   const path = pathFinder.findPath(nearestStartPointOnLine, nearestEndPointOnLine)
-  if(!path) console.warn("path not found")
+  if (!path) {
+    console.warn('path not found')
+    return 
+  }
 
   // tranform to LineString object
 
@@ -136,16 +179,26 @@ const updatePath = () => {
 
   // simplify & curve path
 
-  const pathLineStringSimplified = turf.simplify(pathLineString, {tolerance: 0.00003})
-  const pathLineStringCurved = turf.bezierSpline(pathLineStringSimplified, {resolution: 30000, sharpness: 0.3})
+  const pathLineStringSimplified = turf.simplify(pathLineString, {tolerance: configuration.pathSimplification})
+  const pathLineStringCurved = turf.bezierSpline(pathLineStringSimplified, {resolution: 30000, sharpness: configuration.pathCurveSharpness})
 
   // update path on map
 
-  map.getSource("path").setData(pathLineStringCurved);
+  switch (configuration.pathType) {
+    case 'raw':
+      map.getSource('path').setData(pathLineString)
+      break;
+    case 'simplified':
+      map.getSource('path').setData(pathLineStringSimplified)
+      break;
+    case 'curved':
+      map.getSource('path').setData(pathLineStringCurved)
+      break;
+  }
 
   // update info
 
-  const pathLength = turf.length(pathLineStringCurved, {units: "kilometers"})
+  const pathLength = turf.length(pathLineStringCurved, {units: 'kilometers'})
   info.style.display = 'block'
   info.innerHTML = `Distance : ${Math.round(pathLength * 1000)} mètres`
 
@@ -157,5 +210,64 @@ markerStart.on('drag', throttle(updatePath, 100))
 markerEnd.on('drag', throttle(updatePath, 100))
 
 
+// DAT GUI
 
+const gui = new dat.GUI()
 
+gui.add(configuration, 'showTiles')
+  .name('Show tiles')
+  .onChange(value => {
+    map.setLayoutProperty('tiles', 'visibility', value ? 'visible' : 'none')
+  })
+
+// path folder
+
+const pathFolder = gui.addFolder('Path')
+pathFolder.open()
+
+pathFolder.add(configuration, 'pathType', ['raw', 'simplified', 'curved'])
+  .name('Path type')
+  .onChange(() => {
+    updatePath()
+  })
+
+pathFolder.add(configuration, 'pathWidth', 0, 16, 1)
+  .name('Path width')
+  .onChange((value) => {
+    map.setPaintProperty('path', 'line-width', value)
+  })
+
+pathFolder.addColor(configuration, 'pathColor')
+  .name('Path color')
+  .onChange((value) => {
+    map.setPaintProperty('path', 'line-color', value)
+  })
+
+pathFolder.add(configuration, 'pathSimplification', 0, 0.00015, 0.000005)
+  .name('Path simplify')
+  .onChange(throttle(() => {
+    updatePath()
+  }, 100))
+
+pathFolder.add(configuration, 'pathCurveSharpness', 0, 1, 0.05)
+  .name('Path curve')
+  .onChange(throttle(() => {
+    updatePath()
+  }, 100))
+
+// routes folder
+
+const routesFolder = gui.addFolder('Routes')
+routesFolder.open()
+
+routesFolder.add(configuration, 'routesWidth', 0, 10, 1)
+  .name('Routes width')
+  .onChange((value) => {
+    map.setPaintProperty('routes', 'line-width', value)
+  })
+
+routesFolder.addColor(configuration, 'routesColor')
+  .name('Routes color')
+  .onChange((value) => {
+    map.setPaintProperty('routes', 'line-color', value)
+  })
